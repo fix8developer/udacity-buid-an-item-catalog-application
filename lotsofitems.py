dcf273256a2a5fbf8aa0db75afcf34611b304f98
FixEight <kashifiqbal23@gmail.com>
2018-11-26 22:24:58 +0500

    Set: Database engine for postgresql in lotsofitems.py

--- lotsofitems.py
+++ lotsofitems.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Categories, Base, Items, User
 
-engine = create_engine('sqlite:///catalog.db')
+#engine = create_engine('sqlite:///catalog.db')
+engine = create_engine(
+    'postgresql+psycopg2://catalog:catalog@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
